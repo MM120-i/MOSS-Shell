f5ca2edb111cdf4336e7cfc8251d405de66a0de7
Mahim <mahimmarufuzzaman1234@gmail.com>
2026-03-28 00:50:43 -0400

    inputssssss

--- include/input.h
+++ include/input.h
@@ -1,3 +1,9 @@
 #pragma once
 
+#include <termios.h>
+
 #define private static
+
+void moss_input_init();
+void moss_input_restore();
+char *moss_input_readline(const char *);
--- src/modules/input.c
+++ src/modules/input.c
@@ -1,1 +1,16 @@
 #include "include/input.h"
+
+// todo
+void moss_input_init()
+{
+}
+
+// todo
+void moss_input_restore()
+{
+}
+
+// todo
+char *moss_input_readline(const char *prompt)
+{
+}
